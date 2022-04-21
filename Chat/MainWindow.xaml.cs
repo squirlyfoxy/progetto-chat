2f398c2c13120c0e376d371923dd40693446dd05
squirlyfoxy <public.thesquirlyz97@gmail.com>
2022-04-21 22:03:45 +0200

    Migliaramento millisecondi di aggiornamento per diminuire l'effort sulla CPU, titolo della finestra, messaggi inviati ora visualizzati nella listbox, gestione errori

--- Chat/MainWindow.xaml.cs
+++ Chat/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace Chat
     public partial class MainWindow : Window
     {
         // Costanti
-        const int MILLISECONDS = 250;
+        const int UPDATE_MILLISECONDS = 500;
 
         // Oggetti
         Socket          mSocket = null;
@@ -57,48 +57,70 @@ namespace Chat
 
             mTimer = new DispatcherTimer();
 
-            mTimer.Tick     +=  new EventHandler(aggiorna_messaggi);    // Evento da eseguire quando passa il tempo
-            mTimer.Interval =   new TimeSpan(0, 0, 0, 0, MILLISECONDS); // Ogni quanto eseguiure il metodo?
-            mTimer.Start();                                             // Avvia il timer
+            mTimer.Tick     +=  new EventHandler(aggiorna_messaggi);            // Evento da eseguire quando passa il tempo
+            mTimer.Interval =   new TimeSpan(0, 0, 0, 0, UPDATE_MILLISECONDS);  // Ogni quanto eseguiure il metodo?
+            mTimer.Start();                                                     // Avvia il timer
 
             InitializeComponent();
+
+            // ************************************
+            // Inizializzazione componenti grafica
+
+            this.Title = "Chat - " + System.Environment.MachineName;
         }
 
         private void aggiorna_messaggi(object sender, EventArgs e)
         {
             int nBytesAvailable = 0; // Numero di bytes ricevuti
 
-            if ((nBytesAvailable = mSocket.Available) > 0) // Se ci sono bytes da processsare (nBytes diventa il numero di bytes)
+            try
             {
-                int     nBytes = 0; //Numero di bytes effettivamente ricevuti
-                string  sender_ip = "";
-                string  message = "";
-                byte[]  recieved_bytes = new byte[nBytesAvailable];   // Creo il buffer dove inseriremo i bytes ricevuti
+                if ((nBytesAvailable = mSocket.Available) > 0) // Se ci sono bytes da processsare (nBytes diventa il numero di bytes)
+                {
+                    int     nBytes = 0; //Numero di bytes effettivamente ricevuti
+                    string  sender_ip = "";
+                    string  message = "";
+                    byte[]  recieved_bytes = new byte[nBytesAvailable];   // Creo il buffer dove inseriremo i bytes ricevuti
 
-                EndPoint sender_endpoint = new IPEndPoint(IPAddress.Any, 0);    // Non so chi sia chi ha inviato il messaggio
+                    EndPoint sender_endpoint = new IPEndPoint(IPAddress.Any, 0);    // Non so chi sia chi ha inviato il messaggio
 
-                nBytes      = mSocket.ReceiveFrom(recieved_bytes, ref sender_endpoint);     // Ricevo il messaggio, imposto sender_endpoint
-                sender_ip   = ((IPEndPoint)(sender_endpoint)).Address.ToString();           // Qual'è l'ip del mittente?
-                message     = Encoding.UTF8.GetString(recieved_bytes, 0, nBytes);           // Finalmente il messaggio
+                    nBytes      = mSocket.ReceiveFrom(recieved_bytes, ref sender_endpoint);     // Ricevo il messaggio, imposto sender_endpoint
+                    sender_ip   = ((IPEndPoint)(sender_endpoint)).Address.ToString();           // Qual'è l'ip del mittente?
+                    message     = Encoding.UTF8.GetString(recieved_bytes, 0, nBytes);           // Finalmente il messaggio
 
-                // Scrivo il messaggio nella listbox
-                lstMessaggi.Items.Add(
-                        sender_ip + " -> " + message
-                    );
+                    // Scrivo il messaggio nella listbox
+                    lstMessaggi.Items.Add(
+                            sender_ip + " -> " + message
+                        );
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
 
         private void btnInvias_Click(object sender, RoutedEventArgs e)
         {
-            IPAddress   remote_address  = IPAddress.Parse(txtIP.Text);                                             // Destinatario
-            IPEndPoint  remote_endpoint = new IPEndPoint(remote_address.MapToIPv4(), int.Parse(txtPorta.Text));  // Endpoint del destinatario
-            // I messaggi saranno inviati all'ip scelto sulla porta scelta
+            try
+            {
+                IPAddress   remote_address  = IPAddress.Parse(txtIP.Text);                                             // Destinatario
+                IPEndPoint  remote_endpoint = new IPEndPoint(remote_address.MapToIPv4(), int.Parse(txtPorta.Text));  // Endpoint del destinatario
+                // I messaggi saranno inviati all'ip scelto sulla porta scelta
 
-            // Conversione del messaggio in un array di byte
-            byte[] byte_msg = Encoding.UTF8.GetBytes(txtMessaggio.Text);    // Uso la codifica UTF-8
+                // Conversione del messaggio in un array di byte
+                byte[] byte_msg = Encoding.UTF8.GetBytes(txtMessaggio.Text);    // Uso la codifica UTF-8
 
-            // Ora posso inviare il messaggio
-            mSocket.SendTo(byte_msg, remote_endpoint);
+                // Ora posso inviare il messaggio
+                mSocket.SendTo(byte_msg, remote_endpoint);
+
+                // Scrivo cos'ho inviato nella listbox
+                lstMessaggi.Items.Add(
+                    txtIP.Text + " <- " + txtMessaggio.Text
+                    );
+            } catch (Exception ex) // Soprattutto per il Parse
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
